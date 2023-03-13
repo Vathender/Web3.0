41ee8a5d5f78d1ffab6cd36756d06388b07e5488
Vathender <bvathender64@gmail.com>
2023-03-13 08:21:13 +0530

    Initial Commit

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,10 @@
+const Loader = () =>{
+    return(
+        <div classname = "flex justify-center items-center py-3">
+          <div className="animate-spin rounded-full h-14 w-14 border-b-2 border-yellow-700"/>
+        </div>
+
+    );
+}
+
+export default Loader;
